8eb251128c36e16d690568c3a2583863958d6482
jrtp22 <jrtp22@gmail.com>
2023-04-27 15:45:25 -0700

    change a few names

--- views/home.jsx
+++ views/home.jsx
@@ -5,15 +5,15 @@ function home () {
     return (
       <Def>
           <main>
-              <h1>Home</h1>
+              <h1>My Page</h1>
               <div>
       <img src="/images/batarang.jpg" alt="Batarang on wall" />
-      <div>
+      <div id="batpic">
         Photo by <a href="https://unsplash.com/@actionvance">ActionVance</a> on <a href="https://unsplash.com/">Unsplash</a>
       </div>
     </div>
               <a href="/places">
-                <button className="btn-primary">Places Page</button>
+                <button className="btn-primary">My Places Page</button>
               </a>
 
           </main>
