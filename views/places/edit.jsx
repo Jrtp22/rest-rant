bffce57bd9ed6aac744c50b0e4cb2d5eaf26320e
jrtp22 <jrtp22@gmail.com>
2023-04-28 01:00:29 -0700

    delete and edit works

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -6,52 +6,65 @@ function Edit({data, index}) {
         <Def>
             <h2>Edit a Place</h2>
             <form action={`/places/${index}?_method=PUT`} method="POST">
-                <div className="form-group">
+                <div className="row">
+                <div className="form-group col-sm-6">
                     <label htmlFor="name">Place Name</label>
                     <input
+                    className="form-group"
                         type="text"
                         id="name"
                         name="name"
                         defaultValue={data.name}
                     />
                 </div>
-                <div className="form-group">
+                <div className="form-group col-sm-6">
                     <label htmlFor="pic">Place Picture</label>
                     <input
+                    className="form-group"
                         type="text"
                         id="pic"
                         name="pic"
                         defaultValue={data.pic}
                     />
                 </div>
-                <div className="form-group">
+                </div>
+                <div className="row">
+                <div className="form-group col-sm-6">
                     <label htmlFor="city">City</label>
                     <input
+                    className="form-group"
                         type="text"
                         id="city"
                         name="city"
                         defaultValue={data.city}
                     />
                 </div>
-                <div className="form-group">
+                <div className="form-group col-sm-6">
                     <label htmlFor="state">State</label>
                     <input
+                    className="form-group"
                         type="text"
                         id="state"
                         name="state"
                         defaultValue={data.state}
                     />
                 </div>
-                <div className="form-group">
+                </div>
+                <div className="row">
+                <div className="form-group col-sm-6">
                     <label htmlFor="cuisines">Cuisines</label>
                     <input
+                    className="form-group"
                         type="text"
                         id="cuisines"
                         name="cuisines"
                         defaultValue={data.cuisines}
                     />
                 </div>
+                </div >
+                <a className="col justify-content-center>">
                 <input className="btn btn-primary" type="submit" />
+                </a>
             </form>
         </Def>
     );
