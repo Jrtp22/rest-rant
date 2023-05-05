7cc2c85274c93db8bb787c1bbca3e0dcde715ff1
jrtp22 <jrtp22@gmail.com>
2023-05-04 21:59:11 -0700

    added showEstablished now im gone to watch guardians of the galaxy v3 :)

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,36 +1,36 @@
 const React = require('react')
 const Def = require('../default')
 
-function show (data, index) {
-    return (
-      <Def>
-        <main>
-        <div className="col-sm-6">
-        <h2>
-        <a >
-          {data.place.name}
-        </a>
-        </h2>
-        <p className="text-center">
-        They serve: {data.place.cuisines}
-        </p>
-        <img src={data.place.pic} alt={data.place.name} />
-        <p className="text-center">
-          Located in {data.place.city}, {data.place.state}
-        </p>
-      </div>
-          <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-          Edit
+function show(data, index) {
+  return (
+    <Def>
+      <main>
+        <div className="row">
+          <div className="col-sm-6">
+            <img src={data.place.pic} alt={data.place.name} />
+            <h3>
+              Located in {data.place.city}, {data.place.state}
+            </h3>
+          </div>
+          <div className="col-sm-6">
+            <h2>Description</h2>
+            <h3>{data.place.showEstablished()}</h3>
+            <h4>Serving {data.place.cuisines}</h4>
+          </div>
+          </div>
+          <a href={`/places/${data.id}/edit`} className="btn btn-warning btn-inline">
+            Edit
           </a>
           <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
             <button type="submit" className="btn btn-danger">
               Delete
             </button>
           </form>
-        </main>
-      </Def>
-    );
+        
+      </main>
+    </Def>
+  );
 }
 
-module.exports = show
+module.exports = show;
 
